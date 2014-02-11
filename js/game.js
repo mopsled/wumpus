d58bd4b53dfe24762b4b64e90e61ea6c98820228
Alec Geatches <throwpi@me.com>
2014-02-11 12:58:12 -0800

    Moved Player into separate file

--- js/game.js
+++ js/game.js
@@ -21,36 +21,3 @@ var Game = {
 		scheduler.add(this.player, true);
 	}
 }
-
-function Player(x, y) {
-	this.coordinates = {x: x, y: y};
-	this.draw();
-}
-
-Player.prototype.draw = function() {
-	Game.display.draw(this.coordinates.x, this.coordinates.y, "@", "#ff0");
-}
-
-Player.prototype.act = function() {
-	Game.engine.lock();
-	window.addEventListener("keydown", this);
-}
-
-Player.prototype.handleEvent = function(e) {
-	var keyMap = {};
-	keyMap[ROT.VK_UP] = 0;
-	keyMap[ROT.VK_RIGHT] = 1;
-	keyMap[ROT.VK_DOWN] = 2;
-	keyMap[ROT.VK_LEFT] = 3;
-	if (!(e.keyCode in keyMap)) return;
-
-	var delta = ROT.DIRS[4][keyMap[e.keyCode]];
-	var newCoordinates = {x: this.coordinates.x + delta[0], y: this.coordinates.y + delta[1]}
-	if (!(Game.map.isAFloorSpace(newCoordinates))) return;
-
-	Game.map.drawAt(this.coordinates.x, this.coordinates.y, Game.display);
-	this.coordinates = newCoordinates;
-	this.draw();
-	window.removeEventListener("keydown", this);
-	Game.engine.unlock();
-}
